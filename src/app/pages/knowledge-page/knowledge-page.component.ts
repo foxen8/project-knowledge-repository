54a7cfead25b13562219f1fa59177b7dc446d967
Fotis <fxenitidis@deloitte.gr>
2025-01-14 15:43:55 +0200

    texts added

--- src/app/pages/knowledge-page/knowledge-page.component.ts
+++ src/app/pages/knowledge-page/knowledge-page.component.ts
@@ -10,12 +10,16 @@ import { HelperService } from 'src/app/services/helper-service/helper.service';
 })
 export class KnowledgePageComponent implements OnInit {
   isAdmin: boolean = true;
-  knowledgeDescr: string = 'Τι είναι γνώσεις, τι κάνουμε κ.λπ. Text επεξήγησης';
+  knowledgeDescr: string =
+    'Οι γνώσεις περιλαμβάνουν τις δεξιότητες, την εξειδίκευση και τις πληροφορίες που έχει αποκτήσει ένας εργαζόμενος από επιμορφώσεις, εμπειρία, πρακτική άσκηση, και απαιτούνται για την αποτελεσματική εκτέλεση των καθηκόντων μίας συγκεκριμένης θέσης. Το γεγονός αυτό τις καθιστά κρίσιμες για τη σωστή λειτουργία των φορέων της Δημόσιας Διοίκησης. Στο πλαίσιο της Δημόσιας Διοίκησης, συγκεκριμένες γνώσεις περιλαμβάνονται στα Γενικά Περιγράμματα θέσης, προκειμένου να περιγράψουν με τον βέλτιστο τρόπο τις απαιτήσεις  κάθε θέσης εργασίας. Οι γνώσεις που απαιτεί κάθε θέση εργασίας περιλαμβάνονται λεπτομερώς στα Γενικά Περιγράμματα θέσης. Το σύνολο των γνώσεων αυτών δημιουργούν δέκα επτά (17) ευρύτερες κατηγορίες γνώσεων, οι οποίες εμφανίζονται στα Προφίλ Ρόλων. Αυτή η δομή διευκολύνει την ανάλυση, την κατανόηση και τη χρήση των γνώσεων για την ενίσχυση της στρατηγικής διαχείρισης Ανθρωπίνου Δυναμικού. Η κατηγοριοποίηση των γνώσεων βασίζεται στην εννοιολογική τους συνάφεια και περιλαμβάνει τις εξής κατηγορίες γνώσεων: <br>  1.  Διοικητική γνώση Δημοσίου <br>  2. Οικονομικά & λογιστικά <br>  3. Γνώση έρευνας, μελετών & διοίκησης έργων <br>  4. Ξένες γλώσσες <br>  5. Εκπαίδευση <br>  6. Υγεία & κοινωνική πρόνοια <br>  7. Ψηφιακές γνώσεις <br>  8. Γνώση χειρισμού & επιμέλειας τεχνικού εξοπλισμού <br>  9. Γνώση νομικού πλαισίου <br>  10. Ασφάλεια & ρίσκο <br>  11. Επικοινωνία & μέσα πολιτισμού <br>  12. Περιβάλλον & βιωσιμότητα <br>  13. Υγιεινή τροφίμων & εστίασης <br>  14. Υγιεινή χώρων <br>  15. Μεταφορές <br>  16. Κατασκευές & μηχανολογία <br> 17. Ηθική & δεοντολογία  ';
   selectedTab: any;
   accordionTabs: any = [];
 
   tooltipContent: string = '';
-  constructor(private apiService: ApiService,private helperService:HelperService) {}
+  constructor(
+    private apiService: ApiService,
+    private helperService: HelperService
+  ) {}
   ngOnInit(): void {
     this.getAccordionTabs();
   }
@@ -38,21 +42,27 @@ export class KnowledgePageComponent implements OnInit {
       cancelButtonText: 'Ακύρωση',
       showCancelButton: true,
     });
-    console.log(tab)
-    console.log(title)
+    console.log(tab);
+    console.log(title);
     if (title) {
-      this.apiService.editKnowledgeTitle(tab.id, title).subscribe((resp) => {
-        this.getAccordionTabs();
-      }, (error) => {
-        this.helperService.errorHandle(error);
-      });
+      this.apiService.editKnowledgeTitle(tab.id, title).subscribe(
+        (resp) => {
+          this.getAccordionTabs();
+        },
+        (error) => {
+          this.helperService.errorHandle(error);
+        }
+      );
     }
   }
   getAccordionTabs() {
-    this.apiService.getKnowledgeCategories().subscribe((resp) => {
-      this.accordionTabs = resp;
-    }, (error) => {
-      this.helperService.errorHandle(error);
-    });
+    this.apiService.getKnowledgeCategories().subscribe(
+      (resp) => {
+        this.accordionTabs = resp;
+      },
+      (error) => {
+        this.helperService.errorHandle(error);
+      }
+    );
   }
 }
